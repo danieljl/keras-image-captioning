4d344ef3c1cb287bfd7a6e8f5d00910aa5bfef62
Daniel Julius Lasiman <dj.lasiman@gmail.com>
2017-05-06 13:57:50 +0700

    Add more assertions in models module's test

--- keras_image_captioning/models_test.py
+++ keras_image_captioning/models_test.py
@@ -6,6 +6,8 @@ from keras.regularizers import l1_l2
 from . import config
 from .models import ImageCaptioningModel
 
+_VOCAB_SIZE = 100
+
 
 class TestImageCaptioningModel(object):
     # TODO Add more comprehensive tests
@@ -13,7 +15,7 @@ class TestImageCaptioningModel(object):
     @pytest.fixture
     def model(self):
         return ImageCaptioningModel(learning_rate=0.01,
-                                    vocab_size=50,
+                                    vocab_size=_VOCAB_SIZE,
                                     embedding_size=10,
                                     rnn_output_size=5,
                                     dropout_rate=0.)
@@ -26,42 +28,48 @@ class TestImageCaptioningModel(object):
         with pytest.raises(ValueError):
             ImageCaptioningModel()
 
-        config.init_vocab_size(100)
+        config.init_vocab_size(_VOCAB_SIZE)
         model = ImageCaptioningModel()
         with pytest.raises(AttributeError):
             model.keras_model
 
-        model.build()
-        assert isinstance(model.keras_model, keras.models.Model)
+        self._build_and_assert(model)
 
     def test_with_explicit_args(self, model):
         with pytest.raises(AttributeError):
             model.keras_model
-        model.build()
-        assert isinstance(model.keras_model, keras.models.Model)
+        self._build_and_assert(model)
 
     def test_arg_bidirectional_rnn(self, model):
         model._bidirectional_rnn = not model._bidirectional_rnn
-        model.build()
-        assert isinstance(model.keras_model, keras.models.Model)
+        self._build_and_assert(model)
 
     def test_arg_rnn_type(self, model):
         model._rnn_type = 'gru' if model._rnn_type == 'lstm' else 'lstm'
-        model.build()
-        assert isinstance(model.keras_model, keras.models.Model)
+        self._build_and_assert(model)
 
     def test_arg_rnn_layers(self, model):
         model._rnn_layers = 2 if model._rnn_layers == 1 else 1
-        model.build()
-        assert isinstance(model.keras_model, keras.models.Model)
+        self._build_and_assert(model)
 
     def test_arg_rnn_layers_and_rnn_type(self, model):
         model._rnn_type = 'gru' if model._rnn_type == 'lstm' else 'lstm'
         model._rnn_layers = 2 if model._rnn_layers == 1 else 1
-        model.build()
-        assert isinstance(model.keras_model, keras.models.Model)
+        self._build_and_assert(model)
 
     def test_arg_l1_reg_and_l2_reg(self, model):
         model._regularizer = l1_l2(0.01, 0.01)
+        self._build_and_assert(model)
+
+    def _build_and_assert(self, model):
         model.build()
+
         assert isinstance(model.keras_model, keras.models.Model)
+
+        input_shape = model.keras_model.input_shape
+        assert len(input_shape[0]) == 4  # batch, height, width, channel
+        assert len(input_shape[1]) == 2  # batch, caption_length
+
+        output_shape = model.keras_model.output_shape
+        assert len(output_shape) == 3  # batch, caption_length, vocab_size
+        assert output_shape[-1] == _VOCAB_SIZE
